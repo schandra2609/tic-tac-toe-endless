3979d985908b7e39eac7712559516cf233f203bb
Sayan Chandra <sayanchandra89@gmail.com>
2025-10-03 22:12:45 +0530

    Updated indentation of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/tic-tac-toe-endless/',
-  server: {
-    port: 3000,
-    host: true,
-    allowedHosts: true
-  }
+	plugins: [react()],
+	base: '/tic-tac-toe-endless/',
+	server: {
+		port: 3000,
+		host: true,
+		allowedHosts: true
+	}
 })
